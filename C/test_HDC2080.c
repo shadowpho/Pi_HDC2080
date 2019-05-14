c8e0ac68ed3434b42b6de327049e5598984c3822
GK <shadowp@gmail.com>
2019-05-13 22:55:51 -0700

    Finished, but didn't test at allwq

---
+++ C/test_HDC2080.c
@@ -0,0 +1,12 @@
+#include "C_HDC2080.h"
+#include <stdio.h>
+
+int main()
+{
+	float temperature, humidity; 
+	if(setup_hdc2080() != 0 ) return 0; 
+	if(read_from_hdc2080(&temperature, &humidity)!=0) return 0;
+
+	printf("T = %f, H = %f", temperature, humidity);
+	return 0;
+}
