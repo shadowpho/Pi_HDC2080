2b183b72aa642f5dabe89a1344223e74eb53b534
Scott Greenlay <scott@greenlay.net>
2019-05-19 21:42:22 +0100

    Small fixes

--- Python/testHDC2080.py
+++ Python/testHDC2080.py
@@ -4,5 +4,5 @@ import Pi_HDC2080
 
 hdc2000 = Pi_HDC2080.Pi_HDC2080()
 
-print "{ temperature : %3.1f, humidity : %3.1f }" %(hdc2000.readTemperature(), hdc2000.readHumidity())
+print "{ \"temperature\" : %3.1f, \"humidity\" : %3.1f }" %(hdc2000.readTemperature(), hdc2000.readHumidity())
 
